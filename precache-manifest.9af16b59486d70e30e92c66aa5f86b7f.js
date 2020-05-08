82e97f98d1a84d15b7a65171358fabc3f0a55b07
gautambitious <mvgt.gautam@gmail.com>
2020-05-08 14:23:30 +0530

    Build

---
+++ precache-manifest.9af16b59486d70e30e92c66aa5f86b7f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a29e53621a573295636eb270f6663e0a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f55d1eab0aafa66e742b",
+    "url": "/static/css/2.f697d436.chunk.css"
+  },
+  {
+    "revision": "035caf1932b53872aae7",
+    "url": "/static/css/main.0b5353c8.chunk.css"
+  },
+  {
+    "revision": "f55d1eab0aafa66e742b",
+    "url": "/static/js/2.621f27ac.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.621f27ac.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "035caf1932b53872aae7",
+    "url": "/static/js/main.1868af5b.chunk.js"
+  },
+  {
+    "revision": "033cdeba948254ea1538",
+    "url": "/static/js/runtime-main.928eb6c7.js"
+  }
+]);
